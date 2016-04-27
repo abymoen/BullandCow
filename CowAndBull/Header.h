4a2e3c87125f84928f0559ce053c50802da12dda
Alex Bymoen <a.bymoen@hotmail.com>
2016-04-27 09:39:43 -0600

    Added a new function playGame to simplify main

--- CowAndBull/Header.h
+++ CowAndBull/Header.h
@@ -3,5 +3,6 @@
 
 using namespace std;	//this allows you to not have to write std:: before every cout
 void printIntro();      //You must "declare" your methods at the top
+void playGame();
 bool userGuess();
 bool userCheck(string);
--- CowAndBull/Main.cpp
+++ CowAndBull/Main.cpp
@@ -6,12 +6,8 @@
 
 
 int main() {
-	bool loop = true;
 	printIntro();
-
-	while (loop) {
-		loop = userGuess();
-	}
+	playGame();
 	return 0;			//exit application
 }
 
@@ -21,6 +17,14 @@ void printIntro() {
 	cout << "Welcome to Bulls and Cows, a fun word game!\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n\n";
 	return;
+}
+
+void playGame() {
+	//loop until the user enters "bye" to end the game
+	bool loop = true;
+	while (loop) {
+		loop = userGuess();
+	}
 }
 bool userGuess() {
 	//get a guess from the user
